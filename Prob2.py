36dbe9b92ece533b9a2db5a9c2f06f92ea113de1
Vikas Shenoy Pete <89056652+vikas-shenoy-p@users.noreply.github.com>
2024-04-11 17:16:21 -0700

    Update Prob2.py

--- Prob2.py
+++ Prob2.py
@@ -1,23 +1,24 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row=[[0 for _ in range(9)] for _ in range(9)]
-        col=[[0 for _ in range(9)] for _ in range(9)]
-        box=[[0 for _ in range(9)] for _ in range(9)]
+        cols=defaultdict(set)
+        rows=defaultdict(set)
+        squares=defaultdict(set)
 
         for i in range(9):
             for j in range(9):
                 if board[i][j]==".":
                     continue
                 
-                pos=int(board[i][j])-1
-                if row[i][pos]==1:
+                elif board[i][j] in rows[i] or board[i][j] in cols[j] or board[i][j] in squares[(i//3,j//3)]:
                     return False
-                row[i][pos]=1
-                if col[j][pos]==1:
-                    return False
-                col[j][pos]=1
-                idx=(i//3)+(j//3)*3
-                if box[idx][pos]==1:
-                    return False
-                box[idx][pos]=1
+                
+                else:
+                    rows[i].add(board[i][j])
+                    cols[j].add(board[i][j])
+                    squares[(i//3,j//3)].add(board[i][j])
+        print(len(cols),len(rows),len(squares))
         return True
+
+
+
+        
